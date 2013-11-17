ab22014213b208388a11ca1877357f1c4bc59d50
chidat <chidat@hawaii.edu>
2013-11-16 15:35:51 -1000

    Merged Jared's code for swipe gestures.

--- src/edu/hawaii/ics414/icalendar/MainActivity.java
+++ src/edu/hawaii/ics414/icalendar/MainActivity.java
@@ -3,19 +3,86 @@ package edu.hawaii.ics414.icalendar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
-    
+	
+    /*start swipe code*/
+	private GestureDetector gestureDetector;
+	  
+	  @SuppressWarnings("deprecation")
+	@Override
+	  public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_main);
+
+	    gestureDetector = new GestureDetector(
+	                      new SwipeGestureDetector());
+	  }
+
+
+	  @Override
+	  public boolean onTouchEvent(MotionEvent event) {
+	    if (gestureDetector.onTouchEvent(event)) {
+	      return true;
+	    }
+	    return super.onTouchEvent(event);
+	  }
+
+	  private void onLeftSwipe() {
+		  setContentView(R.layout.activity_calendar_view);
+	  }
+
+	  private void onRightSwipe() {
+		  setContentView(R.layout.activity_time_picker);
+	  }
+
+	  // Private class for gestures
+	  private class SwipeGestureDetector 
+	          extends SimpleOnGestureListener {
+	    // Properties to control the swipe
+	    private static final int SWIPE_MIN_DISTANCE = 120;
+	    private static final int SWIPE_MAX_OFF_PATH = 200;
+	    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+	    @Override
+	    public boolean onFling(MotionEvent e1, MotionEvent e2,
+	                         float velocityX, float velocityY) {
+	      try {
+	        float diffAbs = Math.abs(e1.getY() - e2.getY());
+	        float diff = e1.getX() - e2.getX();
+
+	        if (diffAbs > SWIPE_MAX_OFF_PATH)
+	          return false;
+	        
+	        // Left swipe
+	        if (diff > SWIPE_MIN_DISTANCE
+	        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+	           MainActivity.this.onLeftSwipe();
+
+	        // Right swipe
+	        } else if (-diff > SWIPE_MIN_DISTANCE
+	        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+	          MainActivity.this.onRightSwipe();
+	        }
+	      } catch (Exception e) {
+	        Log.e("MainActivity", "Error on gestures");
+	      }
+	      return false;
+	    }
+	  }
+	
+	/*end swipe code*/
+	  
     public final static String EXTRA_MESSAGE = "edu.hawaii.ics414.icalendar.MESSAGE";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

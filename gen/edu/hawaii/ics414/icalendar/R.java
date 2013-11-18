674aa1aacc4a1035d8fcba57527b5117d69b0aac
chidat <chidat@hawaii.edu>
2013-11-17 21:15:47 -1000

    adding changed compile files

--- gen/edu/hawaii/ics414/icalendar/R.java
+++ gen/edu/hawaii/ics414/icalendar/R.java
@@ -50,8 +50,7 @@ public final class R {
         public static final int calendar_view=0x7f070000;
         public static final int display_message=0x7f070001;
         public static final int main=0x7f070002;
-        public static final int save_load=0x7f070003;
-        public static final int time_picker=0x7f070004;
+        public static final int time_picker=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
